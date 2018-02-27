98b0f981a8c3610be414cc5535a730f3d3e5281f
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-02-27 23:19:28 +0000

    Added spellcheck logic with tests.

---
+++ SpellChecker/Logic/Spellcheck.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellChecker.Logic
+{
+    public interface ISpellCheck
+    {
+        IBloomFilter Filter { get; }
+        bool WordExists(string word);
+    }
+
+    public class SpellCheck : ISpellCheck
+    {
+        public IBloomFilter Filter { get; }
+
+        public SpellCheck(IBloomFilter filter, IWordList wordList)
+        {
+            Filter = filter;
+
+            foreach (var word in wordList.Words)
+            {
+                Filter.Write(word);
+            }
+        }
+
+        public bool WordExists(string word)
+        {
+            return Filter.Verify(word);
+        }
+    }
+}
---
+++ SpellChecker/Tests/SpellCheckerTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using SpellChecker.Logic;
+
+namespace SpellChecker.Tests
+{
+    [TestFixture]
+    public class SpellCheckerTest
+    {
+        private readonly ISpellCheck _check;
+        private readonly IWordList _wordList;
+
+        public SpellCheckerTest()
+        {
+            _wordList = new WordList(
+                $"{TestContext.CurrentContext.TestDirectory}\\Tests\\words.txt");
+            var filter = new BloomFilter();
+            
+            _check = new SpellCheck(filter, _wordList);
+        }
+
+        [Test]
+        public void EveryBitInMapShouldNotBeTrue()
+        {
+            Assert.IsFalse(_check.Filter.Bitmap.All(bit => bit is true));
+        }
+
+        [Test]
+        public void SpellCheckerVerifiesExistingWords()
+        {
+            var existing = new List<string> { "Abbottstown\'s", "overdries", "whatchamacallit", "uncliched" };
+            foreach (var word in existing)
+            {
+                Assert.IsTrue(_wordList.Words.Contains(word));
+                Assert.IsTrue(_check.WordExists(word));
+            }
+        }
+
+        [Test]
+        public void SpellCheckerRejectsNonExistingWord()
+        {
+            var nonExisting = new List<string> { "Zabeen", "Zubeida", "cliche" };
+            foreach (var word in nonExisting)
+            {
+                Assert.IsFalse(_wordList.Words.Contains(word));
+                Assert.IsFalse(_check.WordExists(word));
+            }
+        }
+    }
+}

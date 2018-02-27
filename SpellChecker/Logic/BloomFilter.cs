df11c065bbccb0286f889ece9900822bae140261
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-02-27 17:08:52 +0000

    Added hash logic to BloomFilter.

--- SpellChecker/Logic/BloomFilter.cs
+++ SpellChecker/Logic/BloomFilter.cs
@@ -1,4 +1,10 @@
-﻿namespace SpellChecker.Logic
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SpellChecker.Logic
 {
     public class BloomFilter
     {
@@ -9,6 +15,29 @@
         {
             Bitmap = new bool[bitmapSize];
             _hashCount = hashCount;
+        }
+
+        public IEnumerable<int> Write(string value)
+        {
+            var hashes = GetHashIndexes(value).ToList();
+            hashes.ForEach(h => Bitmap[h] = true);
+            return hashes;
+        }
+
+        public bool Verify(string value)
+        {
+            return GetHashIndexes(value).All(i => Bitmap[i]);
+        }
+
+        private IEnumerable<int> GetHashIndexes(string value)
+        {
+            var byteArray = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(value));
+
+            // source: https://stackoverflow.com/a/5896716
+            var result = new int[byteArray.Length / sizeof(int)];
+            Buffer.BlockCopy(byteArray, 0, result, 0, result.Length);
+
+            return result.Take(_hashCount);
         }
     }
 }
